0c938f7c84d6208fd61974d985ba6d06fd2295ba
Pedro Lammel <pedro.lammel2001@gmail.com>
2026-09-02 13:55:58 -0300

    Funcionalidades básicas de los animales

--- src/DelfinCautiverio.java
+++ src/DelfinCautiverio.java
@@ -1,4 +1,20 @@
+import java.util.Random;
 
-public class DelfinCautiverio implements Delfin {
+public class DelfinCautiverio implements Delfin{
+    private int saltosHechos = 0;
+
+    @Override
+    public void nadar(){
+        int saltos;
+        Random s = new Random();
+        saltos = s.nextInt(15);
+        System.out.println("Mientras nadaba en el delfinário, el delfin saltó " + saltos + " veces");
+    }
+    public int getSaltosHechos() {
+        return saltosHechos;
+    }
+    public void setSaltosHechos(int saltosHechos) {
+        this.saltosHechos = saltosHechos;
+    }
 
 }
